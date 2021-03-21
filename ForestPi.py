04434d4c9e5124bb7a9dd5f6e225163eaa878e49
SullyWG <54372562+SullyWG@users.noreply.github.com>
2021-03-21 14:06:55 -0500

    Update ForestPi.py

--- ForestPi.py
+++ ForestPi.py
@@ -14,36 +14,42 @@ save_path = '/var/www/html'
 file_name = "index.html"
 theCreate = os.path.join(save_path,file_name)
 
+i = 0
+
 while True:
         # The read_adc function will get the value of the specified channel (0-7).
         value = mcp.read_adc(1)         #Reads ADC channel 1 for MQ-2 Input
         if value > 1000:                #If this value is greater than one, the code returns that there is a fire
                 print('FIRE')
+                i += 1
                 print(value)
-                f = open(theCreate,'w')
-
+                if i >= 5:
+                        f = open(theCreate,'w')
+        
 # Writes file and then creates it in a location for Apache to upload to our website
-                message = """<html>
-                <head></head>
-                <body><p> Fire!</p></body>
-                </html>"""
-                f.write(message)
-                f.close()
+                        message = """<html>
+                        <head></head>
+                        <body><p> Fire!</p></body>
+                        </html>"""
+                        f.write(message)
+                        f.close()
 
 
-                #In case there is no fire the same steps occur but the HTML will say there is no fire
+                        #In case there is no fire the same steps occur but the HTML will say there is no fire
+                else:
+                        print('NO FIRE')
+                        print(value)
+                        f = open(theCreate,'w')
+
+                        message = """<html>
+                        <head></head>
+                        <body><p>No Fire!</p></body>
+                        </html>"""
+
+                        f.write(message)
+                        f.close()
         else:
-                print('NO FIRE')
-                print(value)
-                f = open(theCreate,'w')
-
-                message = """<html>
-                <head></head>
-                <body><p>No Fire!</p></body>
-                </html>"""
-
-                f.write(message)
-                f.close()
+                i=0
         #Pause for half a second.
-        time.sleep(1)
+        time.sleep(60)
 
