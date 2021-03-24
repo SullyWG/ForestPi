e1f827eae625455febf830f2677a96f1e275bff5
SullyWG <54372562+SullyWG@users.noreply.github.com>
2021-03-23 21:59:47 -0500

    Update ForestPi.py

--- ForestPi.py
+++ ForestPi.py
@@ -1,6 +1,40 @@
 import time
 import os
 import Adafruit_MCP3008
+import smtplib
+
+#Email Variables
+SMTP_SERVER = 'smtp.gmail.com' #Email Server (don't change!)
+SMTP_PORT = 587 #Server Port (don't change!)
+GMAIL_USERNAME = 'youremail@email.com' #change this to match your gmail account
+GMAIL_PASSWORD = 'yourPassword'  #change this to match your gmail password
+
+class Emailer:
+    def sendmail(self, recipient, subject, content):
+         
+        #Create Headers
+        headers = ["From: " + GMAIL_USERNAME, "Subject: " + subject, "To: " + recipient,
+                   "MIME-Version: 1.0", "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+ 
+        #Connect to Gmail Server
+        session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+ 
+        #Login to Gmail
+        session.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+ 
+        #Send Email & Exit
+        session.sendmail(GMAIL_USERNAME, recipient, headers + "\r\n\r\n" + content)
+        session.quit
+      
+sender = Emailer()
+ 
+sendTo = 'anotheremail@email.com'
+emailSubject = "There was a Fire"
+emailContent = "There was a Fire at the ForestPi location"
 
 #Defines GPIO pins used to get data, this may change based on your wiring
 CLK  = 18
@@ -33,6 +67,7 @@ while True:
                         </html>"""
                         f.write(message)
                         f.close()
+                        sender.sendmail(sendTo,emailSubject,emailContent)     #Wrote this bit of code to send me an email if there is sustained smoke for over 5 minutes so I know when to check for a fire
 
 
                         #In case there is no fire the same steps occur but the HTML will say there is no fire
